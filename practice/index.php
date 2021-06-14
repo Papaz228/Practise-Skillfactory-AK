19228e53dfb4a417edc4ac9751259d81c4ce0364
Papaz228 <74819372+Papaz228@users.noreply.github.com>
2021-06-14 17:23:47 +0600

    Add files via upload

---
+++ practice/index.php
@@ -0,0 +1,18 @@
+
+
+<?php 
+ $p = 'Приветствую Вас на моей страничке!';
+?>
+
+<?php 
+ $name = 'Jorno';
+ $surname = 'Jovano';
+ $city = 'Rome';
+ $age = 20;
+?>
+
+
+<?php
+include 'main.php';
+?>
+
---
+++ practice/main.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Practice </title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+    
+    <div class="flex-container">
+
+        <div class="header">     
+               <?php include 'logo.inc.php' ?>         
+               <?php include 'menu.inc.php' ?>	   
+        </div>       
+     
+        <div class="about_me">
+         
+          <h1>  <?php  echo $p  ?> </h1>
+
+            <div class="data">
+                <div class="myImg">
+                    <?php  echo '<img src="img/1.jpg" width="400" height="300">'; ?>                    
+                </div>
+
+                <div class="fullname">
+                
+                    <p> Меня зовут 
+                    <?php echo $name, ' ', $surname . '<br>'; 
+                          echo 'город', ' ', $city; ?>                                      
+                    </p> 
+           
+                    <p> Мне
+                    <?php  echo $age;   ?>          
+                    лет </p>
+                    <p> Мы научились создавать переменные </p>
+                    <p> Изучили простые операции с ними </p>
+                </div>
+            </div>
+
+            <div class="knowledge">
+                                   
+                    <?php  include 'knowledge.inc.php'; ?>
+                    <?php   echo $a, ' ', $b, ' ', $c, ' ', $e, ' ', $r; ?> <br>
+                                       
+                    <?php
+                        $a = 10;
+                        $b = 20;
+                        $c = $a + $b;
+                        echo $c;
+                    ?>   <br>                
+                     <?php
+                        echo $d;
+                    ?> <br>
+                    <?php 
+                    	echo 'Look ', $h, ' and ', $z, ' ', $l;
+                    ?>
+
+            </div>
+
+            <div class="article">
+                <p class="text">
+                   <q> Wise men speak because they have something to say; fools because they have to say something.</q>
+                </p>
+            </div>
+        </div>
+
+            <?php include 'footer.inc.php' ?>
+
+    </div>
+
+
+</body>
+</html>
